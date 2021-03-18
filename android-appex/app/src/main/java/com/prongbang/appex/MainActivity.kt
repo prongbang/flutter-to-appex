ebf836ce2ab2c9a85b4940b5b148b8b143dbe44b
prongbang <prongbang@gmail.com>
2021-03-18 17:45:44 +0700

    config flutter

--- android-appex/app/src/main/java/com/prongbang/appex/MainActivity.kt
+++ android-appex/app/src/main/java/com/prongbang/appex/MainActivity.kt
@@ -2,10 +2,23 @@ package com.prongbang.appex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.prongbang.appex.databinding.ActivityMainBinding
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs
 
 class MainActivity : AppCompatActivity() {
+
+	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		setContentView(binding.root)
+
+		binding.flutterCounterButton.setOnClickListener {
+			startActivity(FlutterActivity
+					.withNewEngine()
+					.backgroundMode(FlutterActivityLaunchConfigs.BackgroundMode.transparent)
+					.build(this))
+		}
 	}
 }
